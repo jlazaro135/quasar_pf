f7836360b8bba3e950b4e1ba0a07eed188a28327
Jesus <jlazar135@gmail.com>
2023-07-24 17:23:24 +0200

    se continua con desarrollo

---
+++ src/components/BookingsDialog.vue
@@ -0,0 +1,193 @@
+<template>
+    <q-dialog
+        persistent
+        maximized
+        transition-show="slide-left"
+        transition-hide="slide-right"
+        >
+        <q-card class="bg-white">
+            <q-bar class="bg-white">
+                <q-btn dense flat icon="navigate_before" v-close-popup>
+                Atrás
+                </q-btn>
+                <q-space />
+                <q-icon name="info_outlined" />
+            </q-bar>
+
+            <q-card-section>
+                <div class="text-h6">Detalles del alquiler</div>
+                <div class="row items-center">
+                    <q-avatar>
+                        <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                    </q-avatar>
+                    <span class="q-ml-sm text-weight-bold">
+                        Nombre propietario
+                    </span>
+                    <q-space />
+                    <q-btn class="text-black" :icon="symOutlinedChat" color="grey-3" round/>
+                </div>
+                <div class="row items-center q-mt-sm">
+                    <q-rating
+                        v-model="ratingModel"
+                        size="1.5em"
+                        icon="star"
+                        color="purple-9"
+                        class="q-mr-xs"
+                    />
+                    <span class="text-caption">
+                        (5 opiniones)
+                    </span>
+                    <q-space />
+                    <q-btn class="q-pa-none" :ripple="false" flat no-caps label="Ver Opiniones" />
+                </div>
+                <q-separator />
+                <div class="row items-center q-mt-md">
+                    <span class="text-weight-bold">
+                        Localizador
+                    </span>
+                    <q-space />
+                    <span>
+                        #123456
+                    </span>
+                </div>
+                <div class="row items-center q-mt-lg">
+                    <span class="text-weight-bold">
+                        Estado
+                    </span>
+                    <q-space />
+                    <span>
+                        Confirmada
+                    </span>
+                </div>
+                <div class="column q-mt-lg">
+                    <span class="text-weight-bold">
+                        Fechas
+                    </span>
+                    <span class="text-grey-8 q-mt-xs text-caption">
+                        22/05/23 - 25/05/23
+                    </span>
+                </div>
+                <div class="column q-mt-lg">
+                    <span class="text-weight-bold">
+                        Precio
+                    </span>
+                    <span class="text-grey-8 q-mt-xs text-caption">
+                        120 €
+                    </span>
+                </div>
+                <div class="row q-mt-lg items-center no-wrap">
+                    <div class="column">
+                        <span class="text-weight-bold">
+                            Dirección de la plaza
+                        </span>
+                        <span class="text-grey-8 q-mt-xs text-caption">
+                            Calle Falsa 123, 28952, Madrid
+                        </span>
+                    </div>
+                    <q-space />
+                    <q-btn class="text-black" :icon="symOutlinedAssistantNavigation" color="grey-3" label="Ir" rounded no-caps/>
+                </div>
+                <div class="row q-mt-lg items-center no-wrap">
+                    <div class="column">
+                        <span class="text-weight-bold">
+                            Permiso de acceso para ZBE
+                        </span>
+                        <span class="text-grey-8 q-mt-xs text-caption">
+                            La dirección está marcada como parte de una Zona de Bajas Emisiones. 
+                            El propietario ha realizado la gestión para que la plaza esté asignada a 
+                            la matrícula de tu vehículo.
+                        </span>
+                    </div>
+                    <q-space />
+                    <q-btn class="text-black" color="grey-3" label="Ver" rounded no-caps/>
+                </div>
+                <div class="column q-mt-lg">
+                    <span class="text-weight-bold">
+                        vehículo
+                    </span>
+                    <span class="text-grey-8 q-mt-xs text-caption">
+                        Alfa Romeo año la polca
+                    </span>
+                </div>
+                <div class="column q-mt-lg">
+                    <span class="text-weight-bold">
+                        Matrícula vehículo
+                    </span>
+                    <span class="text-grey-8 q-mt-xs text-caption">
+                        1234 BBB
+                    </span>
+                </div>
+                <div class="column q-mt-lg">
+                    <span class="text-weight-bold">
+                        Distintivo ambiental
+                    </span>
+                    <span class="text-grey-8 q-mt-xs text-caption">
+                        Etiqueta C
+                    </span>
+                </div>
+                <div class="row q-mt-lg items-center no-wrap">
+                    <div class="column">
+                        <span class="text-weight-bold">
+                            Dirección de entrega de llaves/mando 
+                        </span>
+                        <span class="text-grey-8 q-mt-xs text-caption">
+                            Calle Falsa 123, 28952, Madrid
+                        </span>
+                    </div>
+                    <q-space />
+                    <q-btn class="text-black" :icon="symOutlinedAssistantNavigation" color="grey-3" label="Ir" rounded no-caps/>
+                </div>
+                <div class="column q-mt-lg">
+                    <span class="text-weight-bold">
+                        Tamaño de la plaza
+                    </span>
+                    <span class="text-grey-8 q-mt-xs text-caption">
+                        Enorme
+                    </span>
+                </div>
+                <div class="column q-mt-lg">
+                    <span class="text-weight-bold">
+                        Tipo de acceso
+                    </span>
+                    <span class="text-grey-8 q-mt-xs text-caption">
+                        Automático
+                    </span>
+                </div>
+                <div class="column q-mt-lg">
+                    <span class="text-weight-bold">
+                       Tipo de plaza 
+                    </span>
+                    <span class="text-grey-8 q-mt-xs text-caption">
+                        Independiente
+                    </span>
+                </div>
+                <div class="column q-mt-lg">
+                    <span class="text-weight-bold">
+                        Punto de recarga
+                    </span>
+                    <span class="text-grey-8 q-mt-xs text-caption">
+                        No
+                    </span>
+                </div>
+                <div class="column q-mt-lg">
+                    <span class="text-weight-bold">
+                        Detalles adicionales
+                    </span>
+                    <span class="text-grey-8 q-mt-xs text-caption">
+                        Hay que darle un roce a la columna para una experiencia plena.
+                    </span>
+                </div>
+                <div class="text-center q-mt-lg">
+                    <q-btn color="red" flat label="Cancelar Alquiler"/> 
+                </div>
+            </q-card-section>
+        </q-card>   
+    </q-dialog>
+</template>
+
+<script setup>
+import { symOutlinedChat, symOutlinedAssistantNavigation } from '@quasar/extras/material-symbols-outlined'
+
+</script>
+
+
--- src/pages/BookingPage.vue
+++ src/pages/BookingPage.vue
@@ -1,172 +1,216 @@
 <template>
-    <q-header
-    class="text-center bg-white text-black"
-    >
-        <q-toolbar>
-            <q-toolbar-title class="text-weight-bold">
-                Reservas
-            </q-toolbar-title>
-        </q-toolbar>
-    </q-header>
-      <q-tabs
-        v-model="tab"
-        class="text-grey"
-        active-color="purple-9"
-        indicator-color="purple-9"
-      >
-        <q-tab name="reservas" icon="directions_car" label="Como conductor" no-caps />
-        <q-tab name="alquileres" icon="local_parking" label="Como propietario" no-caps />
-      </q-tabs>
+  <q-header class="text-center bg-white text-black">
+    <q-toolbar>
+      <q-toolbar-title class="text-weight-bold"> Reservas </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+  <q-tabs
+    v-model="tab"
+    class="text-grey"
+    active-color="purple-9"
+    indicator-color="purple-9"
+  >
+    <q-tab
+      name="reservas"
+      icon="directions_car"
+      label="Como conductor"
+      no-caps
+    />
+    <q-tab
+      name="alquileres"
+      icon="local_parking"
+      label="Como propietario"
+      no-caps
+    />
+  </q-tabs>
 
-      <q-separator />
+  <q-separator />
 
-      <q-tab-panels v-model="tab" animated>
-        <q-tab-panel name="reservas" class="q-pa-none">
-            <q-list>
-                <q-item clickable v-ripple class="q-pa-md">
-                    <q-item-section>
-                        <q-item-label class="text-weight-bold">Calle falsísis isisima 123</q-item-label>
-                        <q-item-label class="text-caption">
-                            22/02/22 - 24/02/22
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-item-label lines="2" class="bg-warning text-white q-pa-xs rounded-borders">
-                            Pendiente de Pago
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-icon name="navigate_next" />
-                    </q-item-section>
-                </q-item>
-                <q-item clickable v-ripple class="q-pa-md">
-                    <q-item-section>
-                        <q-item-label class="text-weight-bold">Calle falsísis isisima 123</q-item-label>
-                        <q-item-label class="text-caption">
-                            13/03/23 - 25/03/23
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-item-label lines="2" class="bg-green text-white q-pa-xs rounded-borders">
-                            Confirmada
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-icon name="navigate_next" />
-                    </q-item-section>
-                </q-item>
-                <q-item clickable v-ripple class="q-pa-md">
-                    <q-item-section>
-                        <q-item-label class="text-weight-bold">Calle falsísis isisima 123</q-item-label>
-                        <q-item-label class="text-caption">
-                            13/03/23 - 25/03/23
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-icon name="warning" color="warning"></q-icon>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-icon name="navigate_next" />
-                    </q-item-section>
-                </q-item>
-                <q-item clickable v-ripple class="q-pa-md">
-                    <q-item-section>
-                        <q-item-label class="text-weight-bold">Calle falsísis isisima 123</q-item-label>
-                        <q-item-label class="text-caption">
-                            13/03/23 - 25/03/23
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-item-label lines="2" class="bg-red text-white q-pa-xs rounded-borders">
-                            Cancelada
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-icon name="navigate_next" />
-                    </q-item-section>
-                </q-item>
-            </q-list>
-        </q-tab-panel>
+  <q-tab-panels v-model="tab" animated>
+    <q-tab-panel name="reservas" class="q-pa-none">
+      <q-list>
+        <q-item clickable v-ripple class="q-pa-md" @click="handleClick">
+          <q-item-section>
+            <q-item-label class="text-weight-bold"
+              >Calle falsísis isisima 123</q-item-label
+            >
+            <q-item-label class="text-caption">
+              22/02/22 - 24/02/22
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-item-label
+              lines="2"
+              class="bg-warning text-white q-pa-xs rounded-borders"
+            >
+              Pendiente de Pago
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="navigate_next" />
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple class="q-pa-md">
+          <q-item-section>
+            <q-item-label class="text-weight-bold"
+              >Calle falsísis isisima 123</q-item-label
+            >
+            <q-item-label class="text-caption">
+              13/03/23 - 25/03/23
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-item-label
+              lines="2"
+              class="bg-green text-white q-pa-xs rounded-borders"
+            >
+              Confirmada
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="navigate_next" />
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple class="q-pa-md">
+          <q-item-section>
+            <q-item-label class="text-weight-bold"
+              >Calle falsísis isisima 123</q-item-label
+            >
+            <q-item-label class="text-caption">
+              13/03/23 - 25/03/23
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="warning" color="warning"></q-icon>
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="navigate_next" />
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple class="q-pa-md">
+          <q-item-section>
+            <q-item-label class="text-weight-bold"
+              >Calle falsísis isisima 123</q-item-label
+            >
+            <q-item-label class="text-caption">
+              13/03/23 - 25/03/23
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-item-label
+              lines="2"
+              class="bg-red text-white q-pa-xs rounded-borders"
+            >
+              Cancelada
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="navigate_next" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-tab-panel>
 
-        <q-tab-panel name="alquileres">
-            <q-list>
-                <q-item clickable v-ripple class="q-pa-none q-py-md">
-                    <q-item-section>
-                        <q-item-label class="text-weight-bold">Calle falsísis isisima 123</q-item-label>
-                        <q-item-label class="text-caption">
-                            22/02/22 - 24/02/22
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-item-label lines="2" class="bg-warning text-white q-pa-xs rounded-borders">
-                            Pendiente de Pago
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-icon name="navigate_next" />
-                    </q-item-section>
-                </q-item>
-                <q-item clickable v-ripple class="q-pa-none q-py-md">
-                    <q-item-section>
-                        <q-item-label class="text-weight-bold">Calle falsísis isisima 123</q-item-label>
-                        <q-item-label class="text-caption">
-                            13/03/23 - 25/03/23
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-item-label lines="2" class="bg-green text-white q-pa-xs rounded-borders">
-                            Confirmada
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-icon name="navigate_next" />
-                    </q-item-section>
-                </q-item>
-                <q-item clickable v-ripple class="q-pa-none q-py-md">
-                    <q-item-section>
-                        <q-item-label class="text-weight-bold">Calle falsísis isisima 123</q-item-label>
-                        <q-item-label class="text-caption">
-                            13/03/23 - 25/03/23
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-icon name="warning" color="warning"></q-icon>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-icon name="navigate_next" />
-                    </q-item-section>
-                </q-item>
-                <q-item clickable v-ripple class="q-pa-none q-py-md">
-                    <q-item-section>
-                        <q-item-label class="text-weight-bold">Calle falsísis isisima 123</q-item-label>
-                        <q-item-label class="text-caption">
-                            13/03/23 - 25/03/23
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-item-label lines="2" class="bg-red text-white q-pa-xs rounded-borders">
-                            Cancelada
-                        </q-item-label>
-                    </q-item-section>
-                    <q-item-section side>
-                        <q-icon name="navigate_next" />
-                    </q-item-section>
-                </q-item>
-            </q-list>
-        </q-tab-panel>
-      </q-tab-panels>
+    <q-tab-panel name="alquileres">
+      <q-list>
+        <q-item clickable v-ripple class="q-pa-none q-py-md">
+          <q-item-section>
+            <q-item-label class="text-weight-bold"
+              >Calle falsísis isisima 123</q-item-label
+            >
+            <q-item-label class="text-caption">
+              22/02/22 - 24/02/22
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-item-label
+              lines="2"
+              class="bg-warning text-white q-pa-xs rounded-borders"
+            >
+              Pendiente de Pago
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="navigate_next" />
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple class="q-pa-none q-py-md">
+          <q-item-section>
+            <q-item-label class="text-weight-bold"
+              >Calle falsísis isisima 123</q-item-label
+            >
+            <q-item-label class="text-caption">
+              13/03/23 - 25/03/23
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-item-label
+              lines="2"
+              class="bg-green text-white q-pa-xs rounded-borders"
+            >
+              Confirmada
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="navigate_next" />
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple class="q-pa-none q-py-md">
+          <q-item-section>
+            <q-item-label class="text-weight-bold"
+              >Calle falsísis isisima 123</q-item-label
+            >
+            <q-item-label class="text-caption">
+              13/03/23 - 25/03/23
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="warning" color="warning"></q-icon>
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="navigate_next" />
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple class="q-pa-none q-py-md">
+          <q-item-section>
+            <q-item-label class="text-weight-bold"
+              >Calle falsísis isisima 123</q-item-label
+            >
+            <q-item-label class="text-caption">
+              13/03/23 - 25/03/23
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-item-label
+              lines="2"
+              class="bg-red text-white q-pa-xs rounded-borders"
+            >
+              Cancelada
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="navigate_next" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-tab-panel>
+  </q-tab-panels>
+  <BookingsDialog 
+  v-model="dialog"
+  />
 </template>
-    
-<script>
+
+<script setup>
+import BookingsDialog from 'src/components/BookingsDialog.vue';
 import { ref } from 'vue'
 
-export default {
-  setup () {
-    return {
-      tab: ref('reservas'),
-      innerTab: ref('innerMails'),
-      splitterModel: ref(20)
-    }
-  }
+const tab = ref('reservas')
+const dialog = ref(false)
+
+function handleClick(){
+    console.log('eee')
+    dialog.value = !dialog.value
+    console.log(dialog.value)
 }
+
 </script>
